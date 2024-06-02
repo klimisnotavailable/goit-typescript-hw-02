73e27abedeaf65d2db81ea395d9ba62a85dcc8b7
klimisnotavailable <klimgrigor28@gmail.com>
2024-06-02 12:17:29 +0300

    дз

--- src/images-api.ts
+++ src/images-api.ts
@@ -5,7 +5,18 @@ axios.defaults.headers.common["Authorization"] = `Client-ID ${authorizationKey}`
 axios.defaults.headers.common["Accept-Version"] = "v1";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
-export const getImages = async (searchQuery:string, page:number):Promise<object> => {
+type Image = {
+  id:string;
+  urls:{raw:string,regular:string,thumb:string,full:string,small:string}
+};
+
+type Response = {
+  results:Image[];
+  total:number;
+  total_pages:number;
+}
+
+export const getImages = async (searchQuery:string, page:number):Promise<Response> => {
   const response = await axios.get("search/photos", {
     params: {
       query: searchQuery,
